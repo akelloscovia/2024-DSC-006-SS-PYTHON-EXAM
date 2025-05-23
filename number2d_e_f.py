80541cc74aecc7ab506fda7bb04c106c4e0b48d2
AKELLO-SCOVIA <akelloscovia907@gmail.com>
2025-05-23 13:10:03 +0300

    number2d_e_f.py

---
+++ number2d_e_f.py
@@ -0,0 +1,22 @@
+#d)
+{
+  "id":11,
+  "name":"Judith",
+  "email":"scovia987@gmail.com",
+  "program_id":1,
+  "courses":[12]
+}
+
+#e)
+DELETE/students/<id>
+
+
+#f)
+{
+  "name":Updated Program Name",
+  "period": 4
+}
+
+
+
+  
